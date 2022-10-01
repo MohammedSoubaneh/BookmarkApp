aa4220d79e2820d097c4fc110f85fc84dba508ba
MohammedSoubaneh <soubcompany@gmail.com>
2022-10-01 14:46:32 -0600

    Success and Error message added to view

--- bookmarks/account/views.py
+++ bookmarks/account/views.py
@@ -1,7 +1,9 @@
+from email import message
 from django.http import HttpResponse
 from django.shortcuts import render
 from django.contrib.auth import authenticate, login
 from django.contrib.auth.decorators import login_required
+from django.contrib import messages
 from .forms import LoginForm, UserRegistrationForm, UserEditForm, ProfileEditForm
 from .models import Profile
 
@@ -66,9 +68,11 @@ def edit(request):
         if user_form.is_valid() and profile_form.is_valid():
             user_form.save()
             profile_form.save()
+            messages.success(request, 'Profile updated successfully')
         else:
             user_form = UserEditForm(instance=request.user)
             profile_form = ProfileEditForm(instance=request.user.profile)
+            messages.error(request, 'Error updating your profile')
         return render(request, 'account/edit.html', {
             'user_form': user_form,
             'profile_form': profile_form
